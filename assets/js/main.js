f9114774b32502a0bd87c0c954ffc77c29684895
Lohan ✌️ <45593750+Sybrax@users.noreply.github.com>
2022-01-05 19:01:29 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,39 @@
-const input = document.querySelectorAll("input");
-const pflogo = document.querySelectorAll(".profil-logo");
+$("input").keyup(function (e) {
+  console.log(e.key);
+  if (e.key === "Enter") {
+    this.value = "";
+    this.blur();
+  }
+});
 
-for (let i = 0; i < input.length; i++) {
-  const inp = input[i];
-  inp.addEventListener("keydown", (button) => {
-    if (button.key === "Enter") {
-      inp.value = "";
-      inp.blur();
-    }
-  });
-}
+$(".profil-logo").click(function () {
+  console.log("You clicked on logo !");
+});
 
-for (let i = 0; i < pflogo.length; i++) {
-  const logo = pflogo[i];
-  logo.addEventListener("click", () => {
-    console.log("You clicked on logo !");
+// Version Checker | You can delete
+
+fetch(
+  "https://raw.githubusercontent.com/Sybrax/Discord-User-Card/main/assets/json/version.json"
+)
+  .then((response) => response.json())
+  .then((response) => {
+    $.getJSON("../assets/json/version.json", function (data) {
+      versionUpdate(data, response);
+    });
+  })
+  .catch((err) => {
+    console.log("Version check error : " + err);
   });
+
+function versionUpdate(a, n) {
+  if (a.version < n.version) {
+    $("body").append(function () {
+      const div = document.createElement("div");
+      div.classList.add("updateVersion");
+      div.setAttribute("onClick", "this.remove()");
+      div.innerHTML = `The version you are testing is not up to date (${a.version}), the latest version (${n.version}) is available on <a href="https://github.com/Sybrax/Discord-User-Card">github</a>.`;
+
+      return div;
+    });
+  }
 }
