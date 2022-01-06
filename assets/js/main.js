151193e212c233e3db0424ed8a136448e9e63e17
Lohan ✌️ <45593750+Sybrax@users.noreply.github.com>
2022-01-06 16:35:31 +0100

    Version Check API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+const id = "G01-dsc";
+const version = "1.1.1";
+
 $("input").keyup(function (e) {
   console.log(e.key);
   if (e.key === "Enter") {
@@ -9,31 +12,3 @@ $("input").keyup(function (e) {
 $(".profil-logo").click(function () {
   console.log("You clicked on logo !");
 });
-
-// Version Checker | You can delete
-
-fetch(
-  "https://raw.githubusercontent.com/Sybrax/Discord-User-Card/main/assets/json/version.json"
-)
-  .then((response) => response.json())
-  .then((response) => {
-    $.getJSON("../assets/json/version.json", function (data) {
-      versionUpdate(data, response);
-    });
-  })
-  .catch((err) => {
-    console.log("Version check error : " + err);
-  });
-
-function versionUpdate(a, n) {
-  if (a.version < n.version) {
-    $("body").append(function () {
-      const div = document.createElement("div");
-      div.classList.add("updateVersion");
-      div.setAttribute("onClick", "this.remove()");
-      div.innerHTML = `<p>The version you are testing is not up to date (${a.version}), the latest version (${n.version}) is available on <a href="https://github.com/Sybrax/Discord-User-Card">github</a>.</p>`;
-
-      return div;
-    });
-  }
-}
